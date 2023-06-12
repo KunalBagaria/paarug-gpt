1593beb7d7f4f7eb75ac081bf627f5ded1b69e80
Kunal Bagaria <kb24x7@gmail.com>
2023-06-12 23:07:45 +0530

    Revert "refactor"
    This reverts commit d4ef93b05f0da47e009027b9e4ead53e0b675c30.

--- src/index.ts
+++ src/index.ts
@@ -42,60 +42,62 @@ client.once(Events.ClientReady, c => {
 });
 
 client.on(Events.MessageCreate, async (message) => {
-  if (message.channel.type === ChannelType.DM) {
-    if (message.partial) return;
-    let responseReturned = false;
+  // only respond in DMs
+  if (message.channel.type !== ChannelType.DM) return;
 
-    if (message.author.bot) return;
+  if (message.partial) return;
+  let responseReturned = false;
 
-    // typing indicator
-    await message.channel.sendTyping();
+  if (message.author.bot) return;
 
-    // Get latest events
-    const calendarEvents = await getEvents();
+  // typing indicator
+  await message.channel.sendTyping();
 
-    // Get last 3 messages
-    const messages = await message.channel.messages.fetch({ limit: 3 });
+  // Get latest events
+  const calendarEvents = await getEvents();
 
-    function sendTypingIndivator() {
-      setTimeout(() => {
-        // If response is not returned, send typing indicator again
-        if (!responseReturned) message.channel.sendTyping();
-        sendTypingIndivator();
-      }, 10500);
-    }
+  // Get last 3 messages
+  const messages = await message.channel.messages.fetch({ limit: 3 });
 
-    sendTypingIndivator();
-
-    const data = messages.reduce<Message[]>((acc, msg) => {
-      acc.unshift({
-        role: msg.author.bot ? 'assistant' : 'user',
-        content: msg.content,
-      });
-      return acc;
-    }, []);
-
-    let response;
-
-    try {
-      response = await makeRequest(data, calendarEvents);
-      responseReturned = true;
-    } catch (e) {
-      console.error(e);
-      responseReturned = true;
-      message.reply('Something went wrong, please contact: @kunalbagaria');
-    }
-    // trim response.content to 2000 characters or less
-    if (response.content.length > 2000) {
-      response.content = response.content.slice(0, 2000);
-    }
-    const row = getButtons();
-
-    message.reply({
-      content: response.content,
-      components: [row as any]
-    });
+  function sendTypingIndivator() {
+    setTimeout(() => {
+      // If response is not returned, send typing indicator again
+      if (!responseReturned) message.channel.sendTyping();
+      sendTypingIndivator();
+    }, 10500);
   }
+
+  sendTypingIndivator();
+
+  const data = messages.reduce<Message[]>((acc, msg) => {
+    acc.unshift({
+      role: msg.author.bot ? 'assistant' : 'user',
+      content: msg.content,
+    });
+    return acc;
+  }, []);
+
+  let response;
+
+  try {
+    response = await makeRequest(data, calendarEvents);
+    responseReturned = true;
+  } catch (e) {
+    console.error(e);
+    responseReturned = true;
+    message.reply('Something went wrong, please contact: @kunalbagaria');
+  }
+  // trim response.content to 2000 characters or less
+  if (response.content.length > 2000) {
+    response.content = response.content.slice(0, 2000);
+  }
+  const row = getButtons();
+
+  message.reply({
+    content: response.content,
+    components: [row as any]
+  });
+
 });
 
 client.on('interactionCreate', async (interaction) => {
